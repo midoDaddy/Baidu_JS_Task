5dc49692dc8e629f439ed8681677f354c79afe9a
midoDaddy <fisheeep@gmail.com>
2017-09-05 14:44:51 +0800

    init

---
+++ JS_Task43/ImgLayout.js
@@ -0,0 +1,16 @@
+/*
+* @Author: midoDaddy
+* @Date:   2017-08-31 14:46:20
+* @Last Modified by:   midoDaddy
+* @Last Modified time: 2017-09-05 14:43:08
+*/
+var ImgLayout = function(cfg) {
+    
+      
+}
+
+ImgLayout.prototype = {
+
+    constructor: Calender,
+
+}
---
+++ JS_Task43/index.js
@@ -0,0 +1,9 @@
+/*
+* @Author: midoDaddy
+* @Date:   2017-08-31 14:46:09
+* @Last Modified by:   midoDaddy
+* @Last Modified time: 2017-09-05 14:44:11
+*/
+$(function() {
+
+})
