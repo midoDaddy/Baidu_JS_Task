fe101a6325ab668aaf8525ed1f410b379f97e81c
midoDaddy <fisheeep@gmail.com>
2017-09-06 01:00:28 +0800

    finish 1, 3, 4, 5, 6

--- JS_Task43/ImgLayouter.js
+++ JS_Task43/ImgLayouter.js
@@ -2,7 +2,7 @@
 * @Author: midoDaddy
 * @Date:   2017-08-31 14:46:20
 * @Last Modified by:   midoDaddy
-* @Last Modified time: 2017-09-05 23:52:50
+* @Last Modified time: 2017-09-06 00:59:59
 */
 var ImgLayouter = function(cfg) {    
     this.cfg = {
@@ -13,8 +13,7 @@ var ImgLayouter = function(cfg) {
     },
     this.CFG = $.extend(this.cfg, cfg);
     this.imgElems = [];
-    this.init();
-      
+    this.init();     
 }
 
 ImgLayouter.prototype = {
@@ -79,32 +78,170 @@ ImgLayouter.prototype = {
 
     //三张图片
     byThree: function() {
+        var CFG = this.CFG,
+            width = CFG.width,
+            height = CFG.height,
+            firstImg = this.imgElems[0],
+            secondImg = this.imgElems[1],
+            thirdImg = this.imgElems[2],
+            firstImgWidth = width - height/2;
 
+        firstImg.css({
+            top: 0, 
+            left: 0
+        });
+        this.clipImgTo(firstImg, firstImgWidth, height);
+
+        secondImg.css({
+            top: 0, 
+            left: firstImgWidth + 'px'
+        });
+        this.clipImgTo(secondImg, height/2, height/2);
+
+        thirdImg.css({
+            top: height/2 + 'px', 
+            left: firstImgWidth + 'px'
+        });
+        this.clipImgTo(thirdImg, height/2, height/2);
     },
 
     //四张图片
     byFour: function() {
+        var CFG = this.CFG,
+            width = CFG.width,
+            height = CFG.height,
+            firstImg = this.imgElems[0],
+            secondImg = this.imgElems[1],
+            thirdImg = this.imgElems[2],
+            fourthImg = this.imgElems[3];
 
+        firstImg.css({
+            top: 0, 
+            left: 0
+        });
+        this.clipImgTo(firstImg, width/2, height/2);
+
+        secondImg.css({
+            top: 0, 
+            left: width/2 + 'px'
+        });
+        this.clipImgTo(secondImg, width/2, height/2);
+
+        thirdImg.css({
+            top: height/2 + 'px', 
+            left: 0
+        });
+        this.clipImgTo(thirdImg, width/2, height/2);
+
+        fourthImg.css({
+            top: height/2 + 'px', 
+            left: width/2 + 'px'
+        });
+        this.clipImgTo(fourthImg, width/2, height/2);
     },
 
     //五张图片
     byFive: function() {
+        var CFG = this.CFG,
+            width = CFG.width,
+            height = CFG.height,
+            firstImg = this.imgElems[0],
+            secondImg = this.imgElems[1],
+            thirdImg = this.imgElems[2],
+            fourthImg = this.imgElems[3],
+            fifthImg = this.imgElems[4];
 
+        firstImg.css({
+            top: 0, 
+            left: 0
+        });
+        this.clipImgTo(firstImg, width*2/3, height*2/3);
+
+        secondImg.css({
+            top: height*2/3 + 'px', 
+            left: 0
+        });
+        this.clipImgTo(secondImg, width/3, height/3);
+
+        thirdImg.css({
+            top: height*2/3 + 'px', 
+            left: width/3 + 'px'
+        });
+        this.clipImgTo(thirdImg, width/3, height/3);
+
+        fourthImg.css({
+            top: 0, 
+            left: width*2/3 + 'px'
+        });
+        this.clipImgTo(fourthImg, width/3, width/3);
+
+        fifthImg.css({
+            top: width/3 + 'px', 
+            left: width*2/3 + 'px'
+        });
+        this.clipImgTo(fifthImg, width/3, height - width/3);
     },
 
     //六张图片
     bySix: function() {
+        var CFG = this.CFG,
+            width = CFG.width,
+            height = CFG.height,
+            firstImg = this.imgElems[0],
+            secondImg = this.imgElems[1],
+            thirdImg = this.imgElems[2],
+            fourthImg = this.imgElems[3],
+            fifthImg = this.imgElems[4],
+            sixthImg = this.imgElems[5];
 
+        firstImg.css({
+            top: 0, 
+            left: 0
+        });
+        this.clipImgTo(firstImg, width*2/3, height*2/3);
+
+        secondImg.css({
+            top: height*2/3 + 'px', 
+            left: 0
+        });
+        this.clipImgTo(secondImg, width/3, height/3);
+
+        thirdImg.css({
+            top: height*2/3 + 'px', 
+            left: width/3 + 'px'
+        });
+        this.clipImgTo(thirdImg, width/3, height/3);
+
+        fourthImg.css({
+            top: 0, 
+            left: width*2/3 + 'px'
+        });
+        this.clipImgTo(fourthImg, width/3, height/3);
+
+        fifthImg.css({
+            top: height/3 + 'px', 
+            left: width*2/3 + 'px'
+        });
+        this.clipImgTo(fifthImg, width/3, height/3);
+
+        sixthImg.css({
+            top: height*2/3 + 'px', 
+            left: width*2/3 + 'px'
+        });
+        this.clipImgTo(sixthImg, width/3, height/3);
     },
 
     //剪切图片至目标尺寸
     clipImgTo: function(el, width, height) {
+
         var elWidth, elHeight, heightDiff, widthDiff, 
             clipTop, clipBottom, clipLeft, clipRight;
+
         el.on('load', function() {
             var self = $(this);
             elWidth = $(this).width();
             elHeight = $(this).height();
+
             if (elWidth/elHeight < width/height) {
                 self.width(width);
                 self.height(width*elHeight/elWidth);
@@ -113,7 +250,7 @@ ImgLayouter.prototype = {
                 clipBottom = self.height() - heightDiff/2 + 'px';
                 self.css({
                     clip: 'rect(' + clipTop + ',' + width + 'px,' + clipBottom + ',0)',
-                    top: '-' + clipTop
+                    top: parseInt(self.css('top'), 10) - heightDiff/2 + 'px'
                 });
             } else {
                 self.height(height);
@@ -123,13 +260,10 @@ ImgLayouter.prototype = {
                 clipRight = self.width() - widthDiff/2 + 'px';
                 self.css({
                     clip: 'rect(0,' + clipRight + ',' + height + 'px,' + clipLeft + ')',
-                    left: '-' + clipLeft
+                    left: parseInt(self.css('left'), 10) - widthDiff/2 + 'px'
                 });
             }
-        })
-        
-
-        
+        })        
     }
 
 }
--- JS_Task43/index.js
+++ JS_Task43/index.js
@@ -2,13 +2,18 @@
 * @Author: midoDaddy
 * @Date:   2017-08-31 14:46:09
 * @Last Modified by:   midoDaddy
-* @Last Modified time: 2017-09-05 23:51:55
+* @Last Modified time: 2017-09-06 00:57:07
 */
 $(function() {
     var imgOne = new ImgLayouter({
         container: $('#wrapper'),
         data: [
-            {index: 1, url: 'images/sample_2.jpg'},
+            {index: 1, url: 'images/sample_1.jpg'},
+            {index: 2, url: 'images/sample_2.jpg'},
+            {index: 3, url: 'images/sample_3.jpg'},
+            {index: 4, url: 'images/sample_4.jpg'},
+            {index: 5, url: 'images/sample_5.jpg'},
+            {index: 6, url: 'images/sample_6.jpg'},
         ]
     })
 })
