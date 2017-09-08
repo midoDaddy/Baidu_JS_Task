33dc671da87d604eaec75a6350fe8c5042f183a7
midoDaddy <fisheeep@gmail.com>
2017-09-08 23:17:04 +0800

    improve annotation

--- JS_Task45/BucketLayout.js
+++ JS_Task45/BucketLayout.js
@@ -2,7 +2,7 @@
 * @Author: midoDaddy
 * @Date:   2017-08-31 14:46:20
 * @Last Modified by:   midoDaddy
-* @Last Modified time: 2017-09-08 23:04:19
+* @Last Modified time: 2017-09-08 23:15:27
 */
 var BucketLayout = function(cfg) {    
     this.cfg = {
@@ -28,20 +28,21 @@ BucketLayout.prototype = {
     
     //初始化
     init: function() {
-        this.renderWrapper();
+        this.createWrapper();
         this.renderImg(this.CFG.data.init);       
         this.bindEvent();
     },
 
     //渲染UI
-    renderWrapper: function() {
+    createWrapper: function() {
         var CFG = this.CFG,
             self = this;
         this.wrapper = $('<div class="bucket-layout-wrapper"></div>')
             .appendTo(CFG.container)
             .width(CFG.container.width());       
     },
-
+    
+    //渲染图片
     renderImg: function(data) {
         var CFG = this.CFG,
             self = this;
@@ -52,17 +53,25 @@ BucketLayout.prototype = {
             }
         });
     },
-
+    
+    //设置行高
     setRowHeight: function() {
         var CFG = this.CFG,
             totalRatio = 0,
             length = this.rowGroup.length,
             rowHeight;
+
+        //行高 = 总宽度/子元素宽高比之和
         $.each(this.rowGroup, function(index, item) {
             totalRatio += item.initWidth/item.initHeight; 
         });
         rowHeight = (CFG.totalWidth - CFG.padding*2*length)/totalRatio;
-        if (rowHeight <= CFG.maxHeight || rowHeight >= CFG.maxHeight && length === CFG.maxCount) {
+        
+        //如果没超过限制高度，则该高度作为行高
+        //如果超过限制高度，则继续添加图片，减少高度
+        //如果添加的图片数量达到上限，仍然超出限高，则此时的高度作为行高
+        if (rowHeight <= CFG.maxHeight || 
+            rowHeight >= CFG.maxHeight && length === CFG.maxCount) {
             $.each(this.rowGroup, function(index, item) {
                 item.height = rowHeight;                  
             });
@@ -71,7 +80,8 @@ BucketLayout.prototype = {
             this.rowGroup.length = 0;
         }
     },
-
+    
+    //渲染单行图片
     renderRowGroup: function() {
         var html = '';
         $.each(this.rowGroup, function(index, item) {
@@ -80,8 +90,7 @@ BucketLayout.prototype = {
             '</div>';
         });
         $('<div class="row-group">' + html + '</div>').appendTo(this.wrapper)
-            .find('.img-con')
-            .css({
+            .find('.img-con').css({
                 float: 'left',
                 padding: this.CFG.padding
             });
@@ -94,7 +103,6 @@ BucketLayout.prototype = {
         if (scrollTop > maxHeight) {
             this.renderImg(this.CFG.data.plus);
         }
-
     },
 
     //绑定事件
--- JS_Task45/index.js
+++ JS_Task45/index.js
@@ -2,7 +2,7 @@
 * @Author: midoDaddy
 * @Date:   2017-08-31 14:46:09
 * @Last Modified by:   midoDaddy
-* @Last Modified time: 2017-09-08 23:05:28
+* @Last Modified time: 2017-09-08 23:15:35
 */
 $(function() {
 
@@ -53,7 +53,6 @@ $(function() {
             {src: 'images/sample_40.jpg', initWidth: 658, initHeight: 439}
         ]
     }
-
 
     //创建木桶布局
     var bucketLayout = new BucketLayout({
